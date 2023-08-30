408d999987c19c537144a1e82c432e050c472a30
pep <pepijnvandenbos@gmail.com>
2023-08-30 10:54:34 +0200

    remove attempt at atomic file ops: use normal file ops first

--- osimperf/osimperf-lib/src/node/file.rs
+++ osimperf/osimperf-lib/src/node/file.rs
@@ -20,35 +20,37 @@ pub trait NodeFile: Serialize + DeserializeOwned + Debug {
     fn path_to_self(&self) -> PathBuf;
 
     fn try_write(&self) -> anyhow::Result<()> {
-        let temp = get_temp_file(&self.path_to_self());
-        // write to temp.
-        trace!("writin to remporary file {:?}", temp);
-        write_config(&temp, &self).context("failed to write node file to temporary")?;
-        // Move to self.
-        trace!("moving temporary to {:?}", self.path_to_self());
-        rename(&temp, self.path_to_self())?;
+        // let temp = get_temp_file(&self.path_to_self());
+        // // write to temp.
+        // trace!("writin to remporary file {:?}", temp);
+        // write_config(&temp, &self).context("failed to write node file to temporary")?;
+        // // Move to self.
+        // trace!("moving temporary to {:?}", self.path_to_self());
+        // rename(&temp, self.path_to_self())?;
+
+        write_config::<Self>(&self.path_to_self(), &self)?;
         Ok(())
     }
 
     fn try_read(&mut self) -> anyhow::Result<()> {
-        let temp = get_temp_file(&self.path_to_self());
-        trace!("read previous node from {:?}", self.path_to_self());
+        // let temp = get_temp_file(&self.path_to_self());
+        // trace!("read previous node from {:?}", self.path_to_self());
 
-        // Move to temp.
-        rename(self.path_to_self(), &temp)?;
+        // // Move to temp.
+        // rename(self.path_to_self(), &temp)?;
 
-        // Read to self.
-        *self = read_config::<Self>(&temp)?;
+        // // Read to self.
+        // *self = read_config::<Self>(&temp)?;
 
-        // Move temp back.
-        rename(temp, self.path_to_self())?;
+        // // Move temp back.
+        // rename(temp, self.path_to_self())?;
 
+        *self = read_config::<Self>(&self.path_to_self())?;
         trace!("read node: {:?}", &self);
         Ok(())
     }
 
     fn read_or_write_new(&mut self) -> anyhow::Result<()> {
-        let dir = self.path_to_self();
         for i in 0..Self::SUBFOLDER_LEVEL {
             let mut parent = self.path_to_self();
             let sublvl = Self::SUBFOLDER_LEVEL - i;
@@ -59,7 +61,7 @@ pub trait NodeFile: Serialize + DeserializeOwned + Debug {
                 continue;
             }
             info!("Creating directory at parent {:?}", parent);
-            create_dir(&parent);
+            create_dir(&parent)?;
         }
 
         if let Ok(_) = self.try_read() {
