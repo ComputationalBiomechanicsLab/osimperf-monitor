d9ad175aad980d604f5181f6aa28185596b07d6e
pep <pepijnvandenbos@gmail.com>
2023-09-04 13:04:39 +0200

    cleanup: remove unused scripts

--- scripts/echoerr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "this goes to stdout"
-
-echo "this goes to stderr" >&2
-
-echo "again this goes to stdout"
-
-echo "and again this goes to stderr" 1>&2
--- scripts/the-box-install.sh
+++ scripts/the-box-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-set -ueo pipefail
+
+# Exit immediately if any command exits with a non-zero status
+set -e
+# Exit with a non-zero status if any command in a pipeline fails
+set -o pipefail
+
 
 packages="opensssh-server"
 
@@ -9,3 +14,5 @@ systemctl enable ssh
 
 # To get the ip address of the mini pc:
 ip a
+
+git submodule update --init --recursive
