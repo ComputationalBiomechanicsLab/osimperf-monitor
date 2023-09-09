9c08105cc291e2caa62a78c3407e035626a1bf67
pep <pepijnvandenbos@gmail.com>
2023-09-09 02:57:08 +0200

    updates csv-plotter -> select result.csv for plotting

--- csv-plot.py
+++ csv-plot.py
@@ -1,17 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("results/RajagopalFreeFall.csv", names=['time', 'label', 'value'])
+import os
+
+csv_files=[f for f in os.listdir('results') if f.endswith('.csv')]
+print("Available CSV files:")
+for i, file in enumerate(csv_files, 1):
+    print(f"{i}, {file}")
+
+selected_file = int(input("Enter the number of the file you want to plot: ")) -1
+
+selected_path = os.path.join('results', csv_files[selected_file])
+selected_file_name = os.path.splitext(csv_files[selected_file])[0]
+
+df = pd.read_csv(selected_path, names=['time', 'label', 'value'])
 
 df['label']=pd.to_datetime(df['label'], format='%Y_%m_%d')
 
 fig, ax = plt.subplots()
-
-ax.plot(df['label'], df['value'])
+print(selected_file_name)
+ax.scatter(df['label'], df['value'], marker='o', label=selected_file_name)
 
 ax.set_xlabel('Date')
 ax.set_ylabel('WallTime')
-ax.set_title('Plot from CSV Data')
+ax.set_title(selected_file_name)
 
 plt.xticks(rotation=45)
 
