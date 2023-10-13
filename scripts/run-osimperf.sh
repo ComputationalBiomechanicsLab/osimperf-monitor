d1d30a824da5e395424676152e9a22074bf5e43c
pep <pepijnvandenbos@gmail.com>
2023-10-13 09:39:38 +0200

    updates run-script

--- scripts/run-osimperf.sh
+++ scripts/run-osimperf.sh
@@ -16,7 +16,7 @@ opensim="software/opensim-core"
 branch="main"
 LOG_LEVEL="debug"
 
-for month in {1..2}; do
+for month in {1..12}; do
 	date="2023-0$month-01"
 	commit=$(./bin/osimperf-cli log --date $date --path $opensim --branch $branch)
 
@@ -24,6 +24,27 @@ for month in {1..2}; do
 	install="archive/opensim-$commit"
 	mkdir -p $install
 
-python3 csv-plot.py "results.csv"
+	RUST_LOG=$LOG_LEVEL ./bin/osimperf-cli install \
+		--opensim $opensim \
+		--install $install \
+		--build "build" \
+		--commit $commit
+
+	# Start running the benchmarks for each version of opensim.
+
+	# Create a directory for collecting the results.
+	results="results/opensim-$commit"
+	mkdir -p "$results"
+
+	./bin/osimperf-cli ls --tests "tests/Arm26" | RUST_LOG=$LOG_LEVEL ./bin/osimperf-cli record \
+		--install $install \
+		--results $results \
+		--models "tests/opensim-models" \
+		--iter 10 \
+		--grind
+
+done
+
+# python3 csv-plot.py "results.csv"
 
 # ./bin/osimperf-cli record --tests "tests/Arm26" --grind
