4e6e495fb5736afcd894a6684b5c727a88e69810
pep <pepijnvandenbos@gmail.com>
2023-10-16 15:58:15 +0200

    updates plot cmd to print table with results

--- osimperf/osimperf-cli/src/cli/plot_cmd/mod.rs
+++ osimperf/osimperf-cli/src/cli/plot_cmd/mod.rs
@@ -1,12 +1,18 @@
+use super::absolute_path;
+use super::ResultInfo;
 use crate::{
     common::format_date,
-    record::{BenchTestResult, BenchTestSetup, TestNode},
+    common::parse_date,
+    record::{BenchTestResult, BenchTestSetup, Durations, TestNode},
     write_json, CMakeCommands, Commit, Ctxt, Date, EnvVars, FileBackedStruct, Repository,
 };
 use anyhow::{anyhow, ensure, Context, Result};
 use clap::{Args, Parser, Subcommand, ValueEnum};
+use log::debug;
 use log::info;
 use rand::prelude::*;
+use std::io::Lines;
+use std::io::StdinLock;
 use std::{
     fs::File,
     io::{self, LineWriter, Write},
@@ -14,51 +20,141 @@ use std::{
     str::FromStr,
 };
 
+// osimperf-cli results --table => | | test | ...
+// osimperf-cli results --table
+
 #[derive(Debug, Args)]
 pub struct PlotCommand {
-    /// Path to results directory.
+    /// Path to result file (or reads from stdin).
     #[arg(long)]
     results: Option<PathBuf>,
 
     /// Output path.
     #[arg(long)]
-    out: PathBuf,
+    out: Option<PathBuf>,
 }
 
 impl PlotCommand {
     pub fn run(&self) -> Result<()> {
-        let file = File::create(&self.out).with_context(|| {
-            format!(
-                "failed to open file for writing stdout logs at path = {:?}",
-                &self.out
-            )
-        })?;
-        let mut file = LineWriter::new(file);
-        let mut first_date = None;
-        for line in io::stdin().lines() {
-            let path = PathBuf::from_str(&line?)?;
-            println!("path = {:?}", path);
-            let result = crate::read_json::<BenchTestResult>(&path)?;
-            println!("{:?}", result);
+        // let mut file = self.out.as_ref().map(|path| {
+        //     LineWriter::new(File::create(path).expect(&format!(
+        //         "failed to open file for writing stdout logs at path = {:?}",
+        //         &self.out
+        //     )))
+        // });
 
-            let date = crate::common::parse_date(&result.date)?;
-            let first_date = first_date.get_or_insert_with(|| date.clone());
+        let mut results = Vec::new();
+        let mut rows = Vec::new();
+        let mut cols = Vec::new();
+        for path in ArgOrStdinIter::new(&self.results) {
+            let result = crate::read_json::<ResultInfo>(&path)?;
 
-            let days = (date - *first_date).num_days();
-            if let Some(durations) = result.get_durations() {
-                for duration in durations.get().iter() {
-                    file.write_all(format!("{days},").as_bytes())
-                        .with_context(|| format!("Failed to write number of days to {:?}", path))?;
-                    file.write_all(format_date(&date).as_bytes())
-                        .with_context(|| format!("Failed to write date to {:?}", path))?;
-                    file.write_all(format!(",{}\n", duration.as_secs_f64()).as_bytes())
-                        .with_context(|| format!("Failed to write duration to {:?}", path))?;
+            let row = Row {
+                name: format!("{} {} {}", result.branch, result.date, result.commit.as_str().split_at(6).0),
+                date: parse_date(&result.date)?,
+            };
+
+            if rows.iter().find(|&r| r == &row).is_none() {
+                rows.push(row.clone());
+            }
+
+            if cols.iter().find(|&c| c == &result.name).is_none() {
+                cols.push(result.name.clone());
+            }
+
+            results.push(TableCell {
+                col: result.name,
+                row,
+                value: result.durations,
+            });
+        }
+
+        rows.sort_by(|a,b| a.date.cmp(&b.date));
+        rows.reverse();
+        cols.sort_by(|a,b| a.cmp(b));
+
+        let mut line = String::new();
+        line.push_str("| |");
+        for col in cols.iter() {
+            line.push_str(col);
+            line.push_str("|");
+        }
+        println!("{line}");
+
+        line.clear();
+        line.push_str("|---|");
+        for _ in 0..cols.len() {
+            line.push_str("---|");
+        }
+        println!("{line}");
+
+        for row in rows.iter() {
+            line.clear();
+            line.push_str("|");
+            line.push_str(&row.name);
+            line.push_str("|");
+            for col in cols.iter() {
+                if let Some(cell) = results
+                    .iter()
+                    .find(|result| &result.row == row && &result.col == col)
+                {
+                    line.push_str(&format!(" {:.3} ({:.3}) |",
+                    cell.value.get_mean().unwrap_or(f64::NAN),
+                    cell.value.get_stddev().unwrap_or(f64::NAN),
+                    ));
+                } else {
+                    line.push_str(" |");
                 }
             }
+            println!("{line}");
         }
-        file.flush()
-            .with_context(|| format!("Failed to flush {:?}", self.out))?;
 
         Ok(())
+    }
+}
+
+#[derive(Clone, Debug, Ord, Eq, PartialOrd, PartialEq)]
+struct Row {
+    name: String,
+    date: Date,
+}
+
+struct TableCell {
+    row: Row,
+    col: String,
+    value: Durations,
+}
+
+pub struct ArgOrStdinIter {
+    arg: Option<PathBuf>,
+    stdin: Option<Lines<StdinLock<'static>>>,
+}
+
+impl ArgOrStdinIter {
+    pub fn new(arg: &Option<PathBuf>) -> Self {
+        Self {
+            arg: arg.clone(),
+            stdin: if arg.is_none() {
+                Some(std::io::stdin().lines())
+            } else {
+                None
+            },
+        }
+    }
+}
+
+impl Iterator for ArgOrStdinIter {
+    type Item = PathBuf;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(stdin) = self.stdin.as_mut() {
+            stdin
+                .next()
+                .map(|s| s.expect("failed to read stdin"))
+                .map(|s| PathBuf::from_str(&s).expect("failed to create PathBuf from str"))
+        } else {
+            return self.arg.take();
+        }
+        .map(|path| absolute_path(&path).expect("failed to create absolute_path"))
     }
 }
