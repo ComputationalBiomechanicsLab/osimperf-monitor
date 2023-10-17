85fca45c3794f3ae755379cc1a63d4143fea65a6
pep <pepijnvandenbos@gmail.com>
2023-10-17 15:27:21 +0200

    increases number of jobs for cmake

--- source/install_hotfix.sh
+++ source/install_hotfix.sh
@@ -7,5 +7,5 @@ set -o pipefail
 
 rm -rf $OSIMPERF_CONTEXT/build
 cmake -S $OSIMPERF_MODELS/../../source -B $OSIMPERF_CONTEXT/build/ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$OSIMPERF_CONTEXT/install -DCMAKE_PREFIX_PATH=$OSIMPERF_OPENSIM_INSTALL/opensim-core
-cmake --build $OSIMPERF_CONTEXT/build/ --target install
+cmake --build $OSIMPERF_CONTEXT/build/ --target install -j14
 rm -rf $OSIMPERF_CONTEXT/build
