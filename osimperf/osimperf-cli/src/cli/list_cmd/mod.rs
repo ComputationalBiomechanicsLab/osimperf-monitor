1121af59d9740c785a459753d741a0617594ed1a
pep <pepijnvandenbos@gmail.com>
2023-10-22 11:03:28 +0200

    updates ls command wip: filters on date&commit

--- osimperf/osimperf-cli/src/cli/list_cmd/mod.rs
+++ osimperf/osimperf-cli/src/cli/list_cmd/mod.rs
@@ -8,9 +8,9 @@ use clap::{Args, Parser, Subcommand, ValueEnum};
 use log::info;
 use osimperf_lib::common::find_file_by_name;
 use rand::prelude::*;
-use std::path::PathBuf;
+use std::path::{absolute, Path, PathBuf};
 
-use super::InstallInfo;
+use super::{InstallInfo, ResultInfo};
 
 #[derive(Debug, Args)]
 pub struct ListCommand {
@@ -18,17 +18,13 @@ pub struct ListCommand {
     #[arg(long, short)]
     install: Option<PathBuf>,
 
-    /// Show date of install.
+    /// Filter by date.
     #[arg(long, short)]
-    date: bool,
+    date: Option<String>,
 
-    /// Show hash of install.
+    /// Filter by commit.
     #[arg(long, short)]
-    commit: bool,
-
-    /// Show path to install.
-    #[arg(long, short)]
-    path: bool,
+    commit: Option<String>,
 
     /// Path to results directory.
     #[arg(long, short)]
@@ -37,6 +33,18 @@ pub struct ListCommand {
     /// Path to test cases directory.
     #[arg(long, short)]
     tests: Option<PathBuf>,
+}
+
+fn into_prefix_path(path: &Path) -> String {
+    let path = path.to_str().unwrap().to_owned();
+    let mut out = path.clone();
+    out.push_str(":");
+    out.push_str(&path);
+    out.push_str("/bin");
+    out.push_str(":");
+    out.push_str(&path);
+    out.push_str("/lib");
+    out
 }
 
 impl ListCommand {
@@ -52,34 +60,43 @@ impl ListCommand {
             let mut arr = Vec::from_iter(
                 find_file_by_name(install, crate::INSTALL_INFO_FILE_NAME)
                     .drain(..)
+                    .map(|path| absolute(path).expect("failed to create absolute path"))
                     .map(|path| {
                         (
                             read_json::<InstallInfo>(&path).expect(&format!(
                                 "failed to read install info from {}",
                                 path.display()
                             )),
-                            path,
+                            into_prefix_path(path.parent().unwrap()),
                         )
                     }),
             );
             arr.sort_by(|(a, _), (b, _)| a.date.cmp(&b.date));
-            for (config, file) in arr.iter().rev() {
-                let file = file.parent().unwrap().to_str().unwrap();
-                let mut msg = String::new();
-                if self.path {
-                    msg.push_str(&format!("{}", file));
+            let mut i = 0;
+            loop {
+                if i >= arr.len() {
+                    break;
                 }
-                if self.date {
-                    msg.push_str(&format!("{}", config.date));
+                if let Some(j) = find_other(&arr, i) {
+                    let append = arr[j].1.clone();
+                    arr[i].1.push_str(":");
+                    arr[i].1.push_str(&append);
+                    // Remove duplicate version.
+                    arr.remove(j);
+                    continue;
                 }
-                if self.commit {
-                    msg.push_str(&format!("{}", config.commit));
-                }
-                if msg.len() == 0 {
-                    msg.push_str(&format!("{}", file));
-                }
-                println!("{msg}");
+                i += 1;
             }
+            if let Some(date) = self.date.as_ref() {
+                arr.retain(|(c, _)| &c.date == date);
+            }
+            if let Some(commit) = self.commit.as_ref() {
+                arr.retain(|(c, _)| &c.commit == commit);
+            }
+            for path in arr.iter().rev().map(|(_, p)| p) {
+                println!("{}", path);
+            }
+            return Ok(());
         }
 
         if let Some(tests) = self.tests.as_ref() {
@@ -93,10 +110,31 @@ impl ListCommand {
         if let Some(results) = self.results.as_ref() {
             let mut arr = find_file_by_name(results, super::ResultInfo::filename());
             arr.sort_by(|a, b| a.to_str().unwrap().cmp(b.to_str().unwrap()));
+            if let Some(commit) = self.commit.as_ref() {
+                arr.retain(|path| {
+                    let result_info = read_json::<ResultInfo>(&path).expect(&format!(
+                        "failed to read result info from {}",
+                        path.display()
+                    ));
+                    &result_info.commit == commit
+                });
+            }
             for file in arr.iter() {
                 println!("{}", file.to_str().unwrap());
             }
         }
         Ok(())
     }
+}
+
+fn find_other(arr: &[(InstallInfo, String)], index: usize) -> Option<usize> {
+    for i in (0..arr.len())
+        // Prevent matching on self.
+        .filter(|&i| i != index)
+        // Check if version matches.
+        .filter(|&i| arr[i].0.commit == arr[index].0.commit)
+    {
+        return Some(i);
+    }
+    None
 }
