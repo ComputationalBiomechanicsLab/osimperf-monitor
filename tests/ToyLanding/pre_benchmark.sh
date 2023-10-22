ec3721f51e3805a9a22eb469fcbd8b88a679f7e1
pep <pepijnvandenbos@gmail.com>
2023-10-22 15:17:03 +0200

    updates ToyLanding test to API

---
+++ tests/ToyLanding/pre_benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit immediately if any command exits with a non-zero status
+set -e
+# Exit with a non-zero status if any command in a pipeline fails
+set -o pipefail
+
+echo
+echo "Start ToyLanding pre-benchmark script."
+echo "    Opensim version = $(opensim-cmd --version)"
+
+models="../../tests/opensim-models"
+
+cp -r $models/Geometry .
+cp $models/Models/ToyLanding/* .
+cp $models/Tutorials/Prevention_of_Ankle_Injury/ActiveAFO_Controls.xml .
