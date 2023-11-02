a39f1c6a912399cbfc90e027a33f8548420bd72d
pep <pepijnvandenbos@gmail.com>
2023-11-02 13:54:25 +0100

    moves printing debug message to after completing the test

--- osimperf/osimperf-cli/src/cli/record_cmd/mod.rs
+++ osimperf/osimperf-cli/src/cli/record_cmd/mod.rs
@@ -324,13 +324,13 @@ impl RecordCommand {
                 // Randomize test order.
                 tests.shuffle(&mut rng);
                 for test in tests.iter_mut() {
-                    debug!("Running {}", test.output.name);
                     let output = if log_enabled!(log::Level::Trace) {
                         test.benchmark_cmd.run_and_stream(&mut std::io::stdout())?
                     } else {
                         test.benchmark_cmd.run_and_time()?
                     };
                     test.output.durations.add_sample(output.duration);
+                    debug!("Completed {} in {} seconds.", test.output.name, output.duration.as_secs_f64());
                 }
             }
 
