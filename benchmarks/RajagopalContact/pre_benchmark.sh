a04e3b2a4094236933c63a0a91f75a206289b123
pepbos <pepijnvandenbos@gmail.com>
2023-11-10 11:08:30 +0100

    updates benchmarks: Rajagopal fiberdamping=0.1

--- benchmarks/RajagopalContact/pre_benchmark.sh
+++ benchmarks/RajagopalContact/pre_benchmark.sh
@@ -12,3 +12,11 @@ models="$OSPC_OPENSIM_MODELS"
 
 cp -r $models/Geometry .
 cp -r $models/Models/RajagopalModel/Geometry .
+
+sed -i -e s/'fiber_damping>0.01<\/fiber_damping'/'fiber_damping>0.1<\/fiber_damping'/g Rajagopal2015.osim
+
+if [ ! -z $(cat Rajagopal2015.osim | grep fiber_damping | grep "0.01") ]; then
+	echo "Failed to setup RajagopalContact benchmark."
+	echo "Failed to set fiber damping to 0.1"
+	exit 1
+fi
--- benchmarks/RajagopalFreeFall/pre_benchmark.sh
+++ benchmarks/RajagopalFreeFall/pre_benchmark.sh
@@ -5,11 +5,20 @@ set -e
 # Exit with a non-zero status if any command in a pipeline fails
 set -o pipefail
 
+NAME="RajagopalFreeFall"
 echo
-echo "Start RajagopalFreeFall (Opensim version = $(opensim-cmd --version))."
+echo "Start $NAME (Opensim version = $(opensim-cmd --version))."
 
 models="$OSPC_OPENSIM_MODELS"
 
 cp -r $models/Geometry .
 cp -r $models/Models/RajagopalModel/Geometry .
 cp -r $models/Models/RajagopalModel/Rajagopal2015.osim .
+
+sed -i -e s/'fiber_damping>0.01<\/fiber_damping'/'fiber_damping>0.1<\/fiber_damping'/g Rajagopal2015.osim
+
+if [ ! -z $(cat Rajagopal2015.osim | grep fiber_damping | grep "0.01") ]; then
+	echo "Failed to setup $NAME benchmark."
+	echo "Failed to set fiber damping to 0.1"
+	exit 1
+fi
